13602b8912f9d24c2dca7f782ab157951ad41187
sharmasakshi2390-oss <sharmasakshi2390@gmail.com>
2026-06-23 21:39:19 +0530

    fix render port

--- todo-backend/src/server.js
+++ todo-backend/src/server.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import app from "./app.js";
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
