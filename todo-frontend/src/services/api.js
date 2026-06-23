91a9da9377935a5b1c248fc6ab4383a0c33931c5
sharmasakshi2390-oss <sharmasakshi2390@gmail.com>
2026-06-23 22:42:19 +0530

    fix api url

--- todo-frontend/src/services/api.js
+++ todo-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://todo-backend-96ho.onrender.com",
+  baseURL: "https://todo-backend-96ho.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
